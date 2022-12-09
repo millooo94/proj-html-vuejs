42e8df9a7293ed9118b7341b85abd3cb39ed588d
millooo94 <camillomusmeciunict@gmail.com>
2022-12-09 18:35:45 +0100

    refactor shadow

--- src/components/HeaderPageFoodieJournal.vue
+++ src/components/HeaderPageFoodieJournal.vue
@@ -167,6 +167,7 @@ export default {
     transform: translateY(-50%);
     padding: .5em 1.2em;
     border-radius: 5px;
+    box-shadow: rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.05) 0px 8px 32px !important;
   }
 }
   .food-journal {
